499a45eacbd8109f45cac34544595b663c48e254
Nara <nah.dand12@gmail.com>
2019-08-26 13:33:39 -0300

    tentativa 48

---
+++ arquivo2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+int idade;
+printf(“Digite sua idade por gentileza: “);
+scanf(“%d”,&idade);
+if (idade>=18){
+	printf("Voce ja é maior");
+}else{
+
+	printf("Voce é menor");
+}
+
+}
